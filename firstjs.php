12994463dcab772bd9fc4ce3602f56c0c9facf10
tymodi20 <tanmaymodi41463@gmail.com>
2022-02-01 14:43:24 +0530

    js programs

---
+++ firstjs.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <button type="submit" onclick="document.getElementById('demo').innerHTML = Date()"  >click me </button>
+    <h1 id="demo">12</h1>
+<br>
+
+    <button type="submit" onclick="document.getElementById('demo2').innerHTML = 'hello content' "  >click me </button>
+    <p id="demo2">content </p>
+
+ 
+</body>
+</html>
